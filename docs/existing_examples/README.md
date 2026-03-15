9a4aa305348b8408c845f69b48abe82ec56f32ca
Roger Mähler <roger.mahler@hotmail.com>
2026-03-15 13:50:55 +0100

    feat(docs): add SQL examples of existing ID management systems

---
+++ docs/existing_examples/README.md
@@ -0,0 +1,8 @@
+
+This folder contains two examples of existing ID management modules.
+
+1. File docs/existing_examples/sead_ids_allocation.sql contains code used by SEAD Clearinghouse Import system.
+2. File bugs_trace contains table DDL for storing links between SEAD and the external BugsCEP system.
+
+Both of these examples will be replaced by the new SEAD Identity System
+
---
+++ docs/existing_examples/sead_ids_allocation.sql
@@ -0,0 +1,119 @@
+
+create table if not exists sead_utility.system_id_allocations (
+    uuid UUID not null default uuid_generate_v4() primary key,
+    table_name text not null,
+    column_name text not null,
+    submission_identifier text not null,
+    change_request_identifier text not null,
+    external_system_id text null,
+    external_data JSON null,
+    alloc_system_id int not null
+);
+
+call sead_utility.drop_udf('sead_utility', 'get_next_system_id');
+-- select sead_utility.get_next_system_id('tbl_sites', 'site_id') 
+create or replace function sead_utility.get_next_system_id(p_table_name text, p_column_name text) 
+/*
+* Get the next SEAD system id for table "p_table_name" and column "p_column_name".
+* The system id is allocated from the sead_utility.system_id_allocations table.
+* If no system id has been allocated for the table and column, the function will return the maximum value of the column in the table.
+*/
+    returns integer as $udf$
+    declare
+        v_next_id integer;
+        v_query text;
+    begin
+        v_query := format('select max(%s) from %s', quote_ident(p_column_name), quote_ident(p_table_name));
+        -- raise notice '%', v_query;
+        execute v_query into v_next_id;
+
+        select max(system_id) into v_next_id
+        from (
+            select coalesce(max(alloc_system_id), 0) as system_id
+            from sead_utility.system_id_allocations
+            where table_name = p_table_name
+            and column_name = p_column_name
+            union all
+            values (v_next_id)
+        );
+        return v_next_id + 1;
+    end;
+    $udf$ language plpgsql;
+
+call sead_utility.drop_udf('sead_utility', 'allocate_system_id');
+
+create or replace function sead_utility.allocate_system_id(
+    p_submission_identifier text,
+    p_change_request_identifier text,
+    p_table_name text,
+    p_column_name text,
+    p_system_id text = NULL,
+    p_data jsonb = NULL
+) returns integer as $udf$
+    declare 
+        v_next_id int;
+    begin
+
+        v_next_id = sead_utility.get_next_system_id(p_table_name, p_column_name);
+        insert into sead_utility.system_id_allocations (
+            table_name,
+            column_name,
+            submission_identifier,
+            change_request_identifier,
+            external_system_id,
+            external_data,
+            alloc_system_id
+        ) values (
+            p_table_name,
+            p_column_name,
+            p_submission_identifier,
+            p_change_request_identifier,
+            p_system_id,
+            p_data,
+            v_next_id
+        );
+        return v_next_id;
+    end;
+    $udf$ language plpgsql;
+
+create or replace function sead_utility.release_allocated_ids(
+    p_submission_identifier text,
+    p_change_request_identifier text=null,
+    p_table_name text=null,
+    p_column_name text=null
+) 
+    returns void as $udf$
+    begin
+        delete from sead_utility.system_id_allocations
+        where submission_identifier = p_submission_identifier   
+        and (p_change_request_identifier is null or change_request_identifier = p_change_request_identifier)
+        and (p_table_name is null or table_name = p_table_name)
+        and (p_column_name is null or column_name = p_column_name)
+        ;
+    end;
+    $udf$ language plpgsql;
+
+create or replace function sead_utility.get_allocated_id(
+    p_submission_identifier text,
+    p_change_request_identifier text,
+    p_table_name text,
+    p_column_name text,
+    p_system_id text = NULL
+) 
+    returns integer as $udf$
+    declare
+        v_alloc_system_id integer;
+    begin
+
+        select max(alloc_system_id::int)
+            into v_alloc_system_id
+                from sead_utility.system_id_allocations
+                where submission_identifier = p_submission_identifier
+                and change_request_identifier = p_change_request_identifier
+                and table_name = p_table_name
+                and column_name = p_column_name
+                and external_system_id = p_system_id
+        ;
+        return v_alloc_system_id;
+    end;
+    $udf$ language plpgsql;
